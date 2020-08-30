086ea8fa01603c2a1f5c328a86a5fc162c9ff70a
namanvashistha <namanvashistha15@gmail.com>
2020-08-30 22:58:55 +0530

    added different functions in controller for simple create, view and delete tickets

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Ticket;
+
+class TicketController extends Controller
+{
+    public function getAllTickets() {
+      $tickets = Ticket::get()->toJson(JSON_PRETTY_PRINT);
+      return response($tickets, 200);
+    }
+
+    public function createTicket(Request $request) {
+      $ticket = new Ticket;
+      $ticket->title = $request->name;
+      $ticket->user_id = 1;
+      $ticket->save();
+
+      return response()->json([
+        "message" => "ticket record created"
+      ], 201);
+    }
+
+    public function getTicket($id) {
+      if (Ticket::where('id', $id)->exists()) {
+        $ticket = Ticket::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+        return response($ticket, 200);
+      } else {
+        return response()->json([
+          "message" => "Ticket not found"
+        ], 404);
+      }
+    }
+
+    public function updateTicket(Request $request, $id) {
+      if (Ticket::where('id', $id)->exists()) {
+        $ticket = Ticket::find($id);
+
+        $ticket->title = is_null($request->name) ? $ticket->title : $request->name;
+        $ticket->user_id = is_null($request->user) ? $ticket->user_id : $request->user;
+        $ticket->save();
+
+        return response()->json([
+          "message" => "records updated successfully"
+        ], 200);
+      } else {
+        return response()->json([
+          "message" => "Ticket not found"
+        ], 404);
+      }
+    }
+
+    public function deleteTicket ($id) {
+      if(Ticket::where('id', $id)->exists()) {
+        $ticket = Ticket::find($id);
+        $ticket->delete();
+
+        return response()->json([
+          "message" => "records deleted"
+        ], 202);
+      } else {
+        return response()->json([
+          "message" => "Ticket not found"
+        ], 404);
+      }
+    }
+}
---
+++ app/Ticket.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $table = 'tickets';
+
+    // protected $fillable = ['title', 'user_id'];
+}
